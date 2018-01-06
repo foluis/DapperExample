10024e2f5acebd3cc3fcf681b7457d26c84ce15c
Luis Fernando <foluis@hotmail.com>
2018-01-06 14:57:18 -0600

    Commit so change fisical work place

--- DapperExample.DataAccess/Interfaces/IAppUserRepository.cs
+++ DapperExample.DataAccess/Interfaces/IAppUserRepository.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using DapperExample.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DapperExample.DataAccess.Interfaces
 {
     public interface IAppUserRepository
     {
+        Task<IEnumerable<AppUser>> GetAppUsers(int pageNum, int pageSize, string sortColumnName, string sortOrder);
     }
 }
--- DapperExample.DataAccess/Repositories/AppUserRepository.cs
+++ DapperExample.DataAccess/Repositories/AppUserRepository.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using DapperExample.DataAccess.Interfaces;
+using DapperExample.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DapperExample.DataAccess.Repositories
 {
-    public class AppUserRepository
+    public class AppUserRepository : IAppUserRepository
     {
+        public Task<IEnumerable<AppUser>> GetAppUsers(int pageNum, int pageSize, string sortColumnName, string sortOrder)
+        {
+            //using (var connection = _base.GetConnection())
+            //{
+            //    var parameters = new DynamicParameters();
+            //    parameters.Add("@pageNum", pageNum);
+            //    parameters.Add("@pageSize", pageSize);
+            //    parameters.Add("@sortColumnName", sortColumnName);
+            //    parameters.Add("@SortOrder", sortOrder);
+
+            //    var budgetsDtoList = await connection.QueryAsync<PresupuestoDTO>("[dbo].[ObtenerPresupuestos]", parameters, null, null, CommandType.StoredProcedure);
+            //    return _mapper.Map<IEnumerable<PresupuestoDTO>, IEnumerable<Budget>>(budgetsDtoList);
+            //}
+            return null;
+        }
     }
 }
